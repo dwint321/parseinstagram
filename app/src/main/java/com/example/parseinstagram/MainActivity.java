3ed7623c8cb81a4e6e4ec850e4cbe7b15aee79e7
dwint321 <dwint321@gmail.com>
2019-03-05 23:28:04 -0500

    Little bit of cleanup

--- app/src/main/java/com/example/parseinstagram/MainActivity.java
+++ app/src/main/java/com/example/parseinstagram/MainActivity.java
@@ -1,40 +1,17 @@
 package com.example.parseinstagram;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.example.parseinstagram.fragments.ComposeFragment;
 import com.example.parseinstagram.fragments.PostsFragment;
 import com.example.parseinstagram.fragments.ProfileFragment;
-import com.parse.FindCallback;
-import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.File;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
